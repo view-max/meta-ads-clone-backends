8165b3df5516578357fd4d65ce82d9be7e6ea3e9
view-max <yolomakanaki26@gmail.com>
2026-05-24 03:00:50 +0100

    Update server.js

--- server.js
+++ server.js
@@ -6,9 +6,10 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 10000;
 
-// Middleware
+// Middleware (With expanded 50MB limits for high-res gallery uploads)
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // Database Connection
 const mongoURI = process.env.MONGO_URI;
